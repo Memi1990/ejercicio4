667615c914acf1abf29ccdccc6c690ea482404de
Alumno <noemilopezpina@gmail.com>
2022-07-11 17:25:42 +0200

    commit radio button

--- app/src/main/java/com/example/ejercicio4/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio4/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.ejercicio4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.example.ejercicio4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b:ActivityMainBinding
+    private var gendre = R.string.famale
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+
+        b.rbtnFemale.setOnClickListener { gendre = R.string.famale }
+        b.rbtnMale.setOnClickListener { gendre = R.string.male }
     }
+
+//    TODO: navBar, Shared Reference, inten
 }
